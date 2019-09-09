f784fa4430126ad4fa8c5b5824d7f31d41b337c4
Yi-Ting, Zeng <33318685+Diuer@users.noreply.github.com>
2019-09-09 16:53:31 +0800

    Add files via upload

---
+++ precache-manifest.db57db905b03d574ec4013181b86b09c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "928fc707105346662d598efecf549ffb",
+    "url": "/find-restaurant/index.html"
+  },
+  {
+    "revision": "223c1878d7d66ba7c3d6",
+    "url": "/find-restaurant/static/css/main.e28eca19.chunk.css"
+  },
+  {
+    "revision": "fdf4022b3246b63e7186",
+    "url": "/find-restaurant/static/js/2.5e66214c.chunk.js"
+  },
+  {
+    "revision": "223c1878d7d66ba7c3d6",
+    "url": "/find-restaurant/static/js/main.e8ea55e8.chunk.js"
+  },
+  {
+    "revision": "7a1ddb5d921214d8f465",
+    "url": "/find-restaurant/static/js/runtime~main.7a3de752.js"
+  }
+]);
